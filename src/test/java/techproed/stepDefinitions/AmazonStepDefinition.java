e003165ae67cb21ac20b2a75a04c4c338b7c70f6
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-05-07 21:30:01 +0300

    070523

--- src/test/java/techproed/stepDefinitions/AmazonStepDefinition.java
+++ src/test/java/techproed/stepDefinitions/AmazonStepDefinition.java
@@ -5,29 +5,45 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import techproed.pages.AmazonPage;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
 
 public class AmazonStepDefinition {
-    @Given("kullanici_amazon_sayfasina_gider")
-    public void kullanici_amazon_sayfasina_gider() {
-        Driver.getDriver().get(ConfigReader.getProperty("amazon_Url"));
-        
-    }
-
-    @Then("arama_kutusunda_iphone_aratilir")
-    public void arama_kutusunda_iphone_aratilir() {
-        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
-        
-    }
-
+    AmazonPage amazonPage;
     @And("sayfayi_kapatir")
     public void sayfayi_kapatir() {
         Driver.closeDriver();
     }
 
-    @And("arama_kutusunda_iphone_Selenium_aratir")
+    @Given("kullanici_amazon_sayfasina_gider")
+    public void kullanici_amazon_sayfasina_gider() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_Url"));
+    }
+    @Then("arama_kutusunda_iphone_aratilir")
+    public void arama_kutusunda_iphone_aratilir() {
+        amazonPage =new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("iphone", Keys.ENTER);
+        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("iphone", Keys.ENTER);
+    }
+
+    @And("arama_kutusunda_Selenium_aratir")
     public void arama_kutusunda_iphone_selenium_aratir() {
-        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("selenium", Keys.ENTER);
+        amazonPage =new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("selenium", Keys.ENTER);
+
+    }
+
+    @And("arama_kutusunda_java_aratir")
+    public void arama_kutusunda_java_aratir() {
+        amazonPage =new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("java", Keys.ENTER);
+
+    }
+
+    @And("arama_kutusunda_sql_aratir")
+    public void arama_kutusunda_sql_aratir() {
+        amazonPage =new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("sql", Keys.ENTER);
     }
 }
