dc17e26c9c9007c0a1c5213aada2eec1bfd94ba1
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-05-07 21:30:01 +0300

    070523

---
+++ src/test/java/techproed/runners/Runner.java
@@ -0,0 +1,29 @@
+package techproed.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+/*
+Runner class; testNG deki .xml file larda belirttiğimiz class'ları,packageları veya methodları nasıl
+çalıştırıyorsak, Cucumber frameworkundede Runner class'ındaki tags parametresi ile belirttiğimiz
+senaryoyu çalıştırabiliriz
+ */
+
+//Cucumber ile JUnit'in entegre olmasını sağlayan test çalıştırıcı notasyonudur
+@RunWith(Cucumber.class)
+
+//Seneryoların nerede ve nasıl çalışacağı, hangi raporu kullanacağıyla alakalı seçenekleri ayarlarız
+@CucumberOptions(features = "src/test/resources/features/day30_IlkFeature",
+        glue = {"techproed/stepDefinitions"},//Bu parametre ile kodlarımızı yazdığımız stepDefinition
+        //class'ının packege'ını belirtiriz
+        tags = "@gr1 or @sql"  )
+/*
+features ===> features'ların olduğu dosyanın yolunu ver(ContentRoot)
+        glue ====> stepDefinition'ların olduğu dosyanın yolunu ver(Source Root)
+        tags ====> çalıştırmak istediğin grubu yaz
+        :white_check_mark:
+        2
+*/
+
+
+public class Runner {
+}
